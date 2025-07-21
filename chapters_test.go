cb2109c54b7db995236a0f94a300ca22716e6dd1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-07-21 22:23:24 +0900

    Add skipped tests for advanced scenarios and examples

--- chapters_test.go
+++ chapters_test.go
@@ -15,10 +15,35 @@ func TestScenario(t *testing.T) {
 	testutil.RunChapterTests(t, "examples/scenario")
 }
 
+func TestRunners(t *testing.T) {
+	t.Skip("Skip runners for now")
+	testutil.RunChapterTests(t, "examples/runners")
+}
+
+func TestAdvanced(t *testing.T) {
+	t.Skip("Skip advanced for now")
+	testutil.RunChapterTests(t, "examples/advanced")
+}
+
+func TestTestHelpers(t *testing.T) {
+	t.Skip("Skip test-helpers for now")
+	testutil.RunChapterTests(t, "examples/test-helpers")
+}
+
 func TestExprLang(t *testing.T) {
 	testutil.RunChapterTests(t, "examples/expr-lang")
 }
 
 func TestRunnBuiltins(t *testing.T) {
 	testutil.RunChapterTests(t, "examples/runn-builtins")
+}
+
+func TestPractices(t *testing.T) {
+	t.Skip("Skip practices for now")
+	testutil.RunChapterTests(t, "examples/practices")
+}
+
+func TestReferences(t *testing.T) {
+	t.Skip("Skip references for now")
+	testutil.RunChapterTests(t, "examples/references")
 }
